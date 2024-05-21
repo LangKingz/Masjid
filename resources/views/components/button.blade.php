e68f9941d8cbe20569a0a3ff0c684fc3d83b3352
= <=>
2024-05-21 20:56:23 +0700

    genahin dari awal

--- resources/views/components/button.blade.php
+++
@@ -1,4 +0,0 @@
-<!-- resources/views/components/button.blade.php -->
-<button {{ $attributes->merge(['class' => 'px-4 py-2 bg-blue-500 text-white rounded']) }}>
-    {{ $slot }}
-</button>
--- resources/views/components/label.blade.php
+++
@@ -1,4 +0,0 @@
-<!-- resources/views/components/label.blade.php -->
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
-    {{ $slot }}
-</label>
