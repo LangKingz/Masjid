0ce9cecb39abdf01bb903c2634ba4d46002cce65
langkingz <gilanglibna@gmail.com>
2024-05-22 10:12:20 +0700

    style

--- resources/views/admin/layouts/create.blade.php
+++ resources/views/admin/layouts/create.blade.php
@@ -1,3 +1,16 @@
+<style>
+    .btn{
+        padding: 0.8rem;
+        border-radius: 0.5rem;
+        background-color: #3490dc;
+    }
+
+    form{
+        padding: 0.3rem;
+        
+    }
+</style>
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -8,15 +21,18 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="dark:bg-gray-100 overflow-hidden shadow-sm sm:rounded-lg ">
-                <div class="p-6 dark:bg-gray-100 border-b border-gray-100 ">
+                <div class="p-6 dark:bg-gray-700   ">
                     <form action="{{ route('posts.store') }}" method="POST">
                         @csrf
-                        <div class="mt-4">
-                            <x-label for="content" :value="__('Content')" />
-                            <textarea id="content" class="block mt-1 w-full" name="content" required></textarea>
+                        <div class="">
+                            <p class="dark:text-gray-300">Pengumuman</p>
                         </div>
                         <div class="mt-4">
-                            <x-button>
+                            <x-label for="content" :value="__('Content')" />
+                            <textarea id="content" class="block mt-1 w-full rounded-lg" name="content" required></textarea>
+                        </div>
+                        <div class="mt-4">
+                            <x-button class="btn">
                                 {{ __('Save') }}
                             </x-button>
                         </div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,36 @@
 <style>
+    .btn-create {
+        background-color: #3490dc;
+
+    }
+
     .btn {
+        padding: 0.8rem;
+        border-radius: 0.5rem;
+    }
+
+    .btn-warning {
+        background-color: #f6993f;
+    
+    }
+
+    .btn-danger {
+        background-color: #e3342f;
+        padding: 0.6rem ;
+        border-radius: 0.4rem;
+    }
+
+    .card{
+        border-radius: 0.5rem;
+        padding: 1rem
         
+    }
+
+    .card-body{
+        display: flex;
+        flex-direction: column;
+        color: #3490dc
+        background-color: #e3342f
     }
 </style>
 
@@ -20,32 +50,29 @@
             </div>
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mt-3">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <div class="p-6 bg-white dark:bg-gray-800 border-b border-gray-200">
-                        <a href="{{ route('posts.create') }}" class="btn btn-primary p-3 bg-purple-400 rounded-md">Create New Post</a>
-                        <table class="table-auto w-full dark:text-gray-100">
-                            <thead>
-                                <tr>
-                                    <th>Content</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
+                    <div class="mb-6">
+                        <a href="{{ route('posts.create') }}" class="p-10 btn-create btn">Buat Pengumuman</a>
+                    </div>
+
+                    <div class="card p-3 mt-4  bg-white dark:bg-gray-700  dark:text-gray-100">
+                        <div class="card-body w-1/2">
+                            <h1 class="text-2xl font-bold text-gray-50">Pengumuman</h1>
+                            <div class="content flex justify-between">
                                 @foreach ($posts as $post)
-                                <tr>
-                
-                                    <td>{{ $post->content }}</td>
-                                    <td>
-                                        <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-warning">Edit</a>
+                                <div class="mt-3 ">
+                                    <p class="text-gray-600 dark:text-gray-200">{{$post->content}}</p>
+                                </div>
+                                <div class="button">
+                                    <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-warning">Edit</a>
                                         <form action="{{ route('posts.destroy', $post->id) }}" method="POST" style="display:inline-block;">
                                             @csrf
                                             @method('DELETE')
-                                            <button type="submit" class="btn btn-danger">Delete</button>
+                                            <button type="submit" class=" btn-danger">Delete</button>
                                         </form>
-                                    </td>
-                                </tr>
+                                </div>
                                 @endforeach
-                            </tbody>
-                        </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
