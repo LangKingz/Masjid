8dd492ca013b70aee6a9efa807750fbc1eb5d0d5
langkingz <gilanglibna@gmail.com>
2024-05-25 13:49:31 +0700

    test

--- app/Livewire/Runningtext.php
+++ app/Livewire/Runningtext.php
@@ -23,7 +23,7 @@ class Runningtext extends Component
 
     public function refreshPosts()
     {
-        $this->posts = Post::orderBy('created_at', 'desc')->get();
+        $this->posts = Post::where('is_history', false)->get();
     }
 
     public function postAdded($post)
